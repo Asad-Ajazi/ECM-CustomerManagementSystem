264b0e5a646f0edd5b3841e49fec989e197a14f8
Asad Ajazi <AsadAjazi.work@gmail.com>
2019-04-24 21:37:11 +0100

    Added inital properties to customer class

---
+++ ECM.BL/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECM.BL
+{
+    public class Customer
+    {
+        // only this class can set the id. Any caller can get it.
+        public int CustomerId { get; private set; }
+        public string  FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        
+        public string MyProperty
+        {
+            get
+            {
+                return LastName + "," + FirstName;
+            }
+        }
+
+    }
+}
