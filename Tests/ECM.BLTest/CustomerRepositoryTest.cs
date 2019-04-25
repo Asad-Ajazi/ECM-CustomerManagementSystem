36477c495794576efe29ba800f3631aaa2243153
Asad Ajazi <AsadAjazi.work@gmail.com>
2019-04-25 17:22:58 +0100

    Added tests for the CustomerRepository class

---
+++ Tests/ECM.BLTest/CustomerRepositoryTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ECM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ECM.BLTest
+{
+    [TestClass]
+    public class CustomerRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveValid()
+        {
+            // Arrange
+            var customerRepository = new CustomerRepository();
+            var expected = new Customer(1)
+            {
+                EmailAddress = "JSmith@gmail.com",
+                FirstName = "John",
+                LastName = "Smith"
+            };
+            // Act
+            var actual = customerRepository.Retrieve(1);
+
+            // Assert
+            Assert.AreEqual(expected.CustomerId, actual.CustomerId);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
+            Assert.AreEqual(expected.EmailAddress, actual.EmailAddress);
+        }
+    }
+}
