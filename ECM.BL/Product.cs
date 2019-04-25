ae7eea8ef38166142d74a407c4b70f5cb1db7e05
Asad Ajazi <AsadAjazi.work@gmail.com>
2019-04-25 21:00:51 +0100

    Created a EntityBase class for all classes to inherit from.

--- ECM.BL/Product.cs
+++ ECM.BL/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ECM.BL
 {
-    public class Product
+    public class Product : EntityBase
     {
         public Product()
         {
@@ -31,7 +31,7 @@ namespace ECM.BL
         /// Validate the product data.
         /// </summary>
         /// <returns></returns>
-        public bool Validate()
+        public override bool Validate()
         {
             var isValid = true;
 
